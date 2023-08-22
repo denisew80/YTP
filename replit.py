3a1a1db57e6de92db22741da0a55f9ad92340fa0
Yaroslav Koval <yaroslav@koval.page>
2023-08-22 18:24:09 +0300

    Update replit.py

--- replit.py
+++ replit.py
@@ -1,5 +1,5 @@
 import subprocess
 
 # A plague upon Replit and all who have built it
-replit_cmd = "killall -q python3 > /dev/null 2>&1; pip install -r requirements.txt && ./run"
+replit_cmd = "killall -q python3 > /dev/null 2>&1; pip install -r requirements.txt && python3 app.py"
 subprocess.run(replit_cmd, shell=True)
